ba943750eff10f1caba1ade0f2f75b8098730b99
BovkunLeonid <parettool.ua@gmail.com>
2023-07-09 23:48:32 +0300

    finish

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+let number1 = prompt('Hello! Напишіть перше число')
+let number2 = prompt('Напишіть друге число')
+let sum = number1 + number2
+let difference = number1 - number2
+let multiplication = number1 * number2
+let division = number1 / number2
+alert ('сума = ' + sum + ';  різниця = ' + difference + ';  множення = ' + multiplication + ';  дiлення = ' + division)
