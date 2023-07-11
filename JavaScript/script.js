19b0eea53ba908cdf6df453abda0813d4e9a8039
BovkunLeonid <parettool.ua@gmail.com>
2023-07-11 20:42:37 +0300

    add ;

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,12 @@
-let numberA = prompt('Hello! Напишіть перше число')
-let numberB = prompt('Напишіть друге число')
-numberA = Number(numberA)
-numberB = Number(numberB)
-let sum = numberA + numberB
-let difference = numberA - numberB
-let multiplication = numberA * numberB
-let division = numberA / numberB
-alert ('сума = ' + sum + ';  різниця = ' + difference + ';  множення = ' + multiplication + ';  дiлення = ' + division)
+let numberA = prompt('Hello! Напишіть перше число');
+let numberB = prompt('Напишіть друге число');
+
+numberA = Number(numberA);
+numberB = Number(numberB);
+
+let sum = numberA + numberB;
+let difference = numberA - numberB;
+let multiplication = numberA * numberB;
+let division = numberA / numberB;
+
+alert ('сума = ' + sum + ';  різниця = ' + difference + ';  множення = ' + multiplication + ';  дiлення = ' + division);
