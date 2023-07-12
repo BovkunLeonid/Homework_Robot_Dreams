505ee318ad0f3090833e59bb86f19227b870a13e
BovkunLeonid <parettool.ua@gmail.com>
2023-07-12 20:13:38 +0300

    finish

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,53 @@
-let numberA = prompt('Hello! Напишіть перше число');
-let numberB = prompt('Напишіть друге число');
+const mesContinue = 'Ви впевнені, що хочете продовжити операцію?';
+const mesError = 'Error !!!';
 
-numberA = Number(numberA);
-numberB = Number(numberB);
+let number1 = prompt('Hello! Напишіть перше число');
+let number2 = prompt('Напишіть друге число');
 
-let sum = numberA + numberB;
-let difference = numberA - numberB;
-let multiplication = numberA * numberB;
-let division = numberA / numberB;
+let sum = +number1 + +number2;
+let difference = number1 - number2;
+let multiplication = number1 * number2;
+let division = number1 / number2;
 
-alert ('сума = ' + sum + ';  різниця = ' + difference + ';  множення = ' + multiplication + ';  дiлення = ' + division);
+let actionNoDel = (`сума: ${sum};  різниця: ${difference};  множення: ${multiplication};  Помилка!!! на "0" не ділять.`);
+let actionFull = (`сума: ${sum};  різниця: ${difference};  множення: ${multiplication};  дiлення: ${division}.`);
+
+//Version 4.1
+if (number1 === '' || number2 === '') {
+    alert (mesError)
+} else if (number2 == 0){
+        alert (actionNoDel);
+    } else if (number1 < number2) {
+        if (confirm (mesContinue)) {
+            alert (actionFull);
+            }    
+        } else {
+                alert (actionFull);
+        }
+
+//Version 3.1
+/*
+if (number1 === '') {
+    alert (mesError)
+} else {
+    if (number2 === '') {
+        alert (mesError)
+    } else {
+        if (number2 == 0) {
+            alert (actionNoDel);
+        } else {
+            if (number1 < number2) {
+                if (confirm (mesContinue)) {
+                 alert (actionFull);
+                } 
+            } else {
+                 alert (actionFull);
+            }
+        }
+    }
+}
+*/
+
+
+
+
