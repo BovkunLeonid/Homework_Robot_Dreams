a78f616b1813ac707c54d2cc8dcbcc74209daf03
BovkunLeonid <parettool.ua@gmail.com>
2023-07-31 22:29:05 +0300

    finish v2

--- 27_DOM/script.js
+++ 27_DOM/script.js
@@ -8,61 +8,53 @@ const resultElementDifference = document.getElementById('difference');
 const resultElementMultiplication = document.getElementById('multiplication');
 const resultElementDivision = document.getElementById('division');
 
+let mySum;
+let myDifference;
+let myMultiplication;
+let myDivision;
+
+let myArray = [];
+let maxElement = 0;
+
+function maxElementArray(abcArray) {
+    maxElement = abcArray[0];
+    for (let i = 0; i < abcArray.length; i++) {
+        if (abcArray[i] > maxElement) {
+            maxElement = abcArray[i];
+        }
+    }
+};
+
+let textStyle = function(result, element) {
+    element.textContent = result;
+    let opacity = (result / maxElement) * 100;
+    if (opacity < 20) {
+        element.style.opacity = '20%';
+    } else { 
+        element.style.opacity = `${opacity}%`;
+    };
+    if (result < 0) {
+        element.style.color = 'red';
+    } else {
+        element.style.color = 'green';
+    }
+};
+
 submitBtn.onclick = function() {
-    let mySum = Number(number1.value) + Number(number2.value);
-    resultElementSum.textContent = mySum;
-    if (mySum < 0) {
-        resultElementSum.style.color = 'red';
-        resultElementSum.style.opacity = '40%';
-    } else if (mySum > 0 && mySum < 100) {
-        resultElementSum.style.color = 'green';
-        resultElementSum.style.opacity = '60%';
-    } else {
-        resultElementSum.style.color = 'green';
-        resultElementSum.style.opacity = '100%';
-        resultElementSum.style.fontWeight = 'bold';
-    }
+    mySum = Number(number1.value) + Number(number2.value);
+    myDifference = Number(number1.value) - Number(number2.value);
+    myMultiplication = Number(number1.value) * Number(number2.value);
+    myDivision = Number(number1.value) / Number(number2.value);
 
-    let myDifference = Number(number1.value) - Number(number2.value);
-    resultElementDifference.textContent = myDifference;
-    if (myDifference < 0) {
-        resultElementDifference.style.color = 'red';
-        resultElementDifference.style.opacity = '40%';
-    } else if (myDifference > 0 && myDifference < 100) {
-        resultElementDifference.style.color = 'green';
-        resultElementDifference.style.opacity = '60%';
-    } else {
-        resultElementDifference.style.color = 'green';
-        resultElementDifference.style.opacity = '100%';
-        resultElementDifference.style.fontWeight = 'bold';
-    }
+    myArray[0] = mySum;
+    myArray[1] = myDifference;
+    myArray[2] = myMultiplication;
+    myArray[3] = myDivision;
 
-    let myMultiplication = Number(number1.value) * Number(number2.value);
-    resultElementMultiplication.textContent = myMultiplication;
-    if (myMultiplication < 0) {
-        resultElementMultiplication.style.color = 'red';
-        resultElementMultiplication.style.opacity = '40%';
-    } else if (myMultiplication > 0 && myMultiplication < 100) {
-        resultElementMultiplication.style.color = 'green';
-        resultElementMultiplication.style.opacity = '60%';
-    } else {
-        resultElementMultiplication.style.color = 'green';
-        resultElementMultiplication.style.opacity = '100%';
-        resultElementMultiplication.style.fontWeight = 'bold';
-    }
+    maxElementArray(myArray);
 
-    let myDivision = Number(number1.value) / Number(number2.value);
-    resultElementDivision.textContent = myDivision;
-    if (myDivision < 0) {
-        resultElementDivision.style.color = 'red';
-        resultElementDivision.style.opacity = '40%';
-    } else if (myDivision > 0 && myDivision < 100) {
-        resultElementDivision.style.color = 'green';
-        resultElementDivision.style.opacity = '60%';
-    } else {
-        resultElementDivision.style.color = 'green';
-        resultElementDivision.style.opacity = '100%';
-        resultElementDivision.style.fontWeight = 'bold';
-    }
-}
-
+    textStyle(mySum, resultElementSum);
+    textStyle(myDifference, resultElementDifference);
+    textStyle(myMultiplication, resultElementMultiplication);
+    textStyle(myDivision, resultElementDivision);
+};
